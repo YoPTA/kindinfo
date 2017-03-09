16ef2be8d5dc142a82dad31c72473e0c501c85c0
YoPTA <s.nichipurenko@yandex.ru>
2017-03-09 15:13:39 +0300

    Removed routes
    1. Были удалены лишние маршруты из файла с маршрутами.

--- config/routes.php
+++ config/routes.php
@@ -1,66 +1,11 @@
 <?php
 
 return [
+    'main/error' => 'main/error',
+    'main/login' => 'main/login',
+    'main/logout' => 'main/logout',
 
-    'admin/index' => 'admin/index',
-
-    'admin/user_index' => 'adminuser/index',
-    'admin/user_add' => 'adminuser/add',
-    'admin/user_edit' => 'adminuser/edit',
-    'admin/user_password' => 'adminuser/password',
-    'admin/user_flag' => 'adminuser/flag',
-
-
-    'company/company_address_index' => 'company/companyaddressindex',
-    'company/company_address_add' => 'company/companyaddressadd',
-    'company/company_address_edit' => 'company/companyaddressedit',
-    'company/company_address_delete' => 'company/companyaddressdelete',
-
-    'company/company_index' => 'company/companyindex',
-    'company/company_add' => 'company/companyadd',
-    'company/company_edit' => 'company/companyedit',
-    'company/company_delete' => 'company/companydelete',
-    'company/company_ip_address' => 'company/companyipaddress',
-
-    'doka/package' => 'doka/package',
-    'doka/login' => 'doka/login',
-
-    'profile/index' => 'profile/index',
-
-    'site/create' => 'site/create',
-    'site/error' => 'site/error',
-    'site/login' => 'site/login',
-    'site/logout' => 'site/logout',
-    'site/barcode_39' => 'site/barcode39',
-    //'site/test' => 'site/test',
-    'site/choose' => 'site/choose',
-    'site/notification' => 'site/notification',
-
-    'route/view' => 'route/view',
-    'route/send' => 'route/send',
-    'route/receive' => 'route/receive',
-    'route/clear_proxy' => 'route/clearproxy',
-
-    'proxy/person_index' => 'proxy/personindex',
-    'proxy/person_add' => 'proxy/personadd',
-    'proxy/person_view' => 'proxy/personview',
-    'proxy/person_edit' => 'proxy/personedit',
-    'proxy/person_delete' => 'proxy/persondelete',
-
-    'proxy/proxy_add' => 'proxy/proxyadd',
-    'proxy/proxy_edit' => 'proxy/proxyedit',
-    'proxy/proxy_delete' => 'proxy/proxydelete',
-
-    'notification/index' => 'notification/index',
-
-    'package/package_object_add' => 'package/packageobjectadd',
-
-    'package/package_add' => 'package/packageadd',
-    'package/objects' => 'package/objects',
-
-    //'site/select_company' => 'site/selectcompany',
-
-    'site/index' => 'site/index',
-    'site' => 'site/index',
-    '' => 'site/index',
+    'main/index' => 'main/index',
+    'main' => 'main/index',
+    '' => 'main/index',
 ];
