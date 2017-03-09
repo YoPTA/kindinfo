511ccd65fad17c405d6d17a374ae50975d12d971
YoPTA <s.nichipurenko@yandex.ru>
2017-03-09 15:38:01 +0300

    Autoload changed // index.php changed
    1. Был изменен класс с автозагрузками. Изменилась логика загрузки файлов
    с классами. Были введены пространства имен.
    2. Изменился файл index.php в соответствии с новыми изменениями.

--- components/Autoload.php
+++ components/Autoload.php
@@ -3,7 +3,7 @@
 /**
  * Функция __autoload для автоматического подключения классов
  */
-function __autoload($class_name)
+/*function __autoload($class_name)
 {
     // Массив папок, в которых могут находиться необходимые классы
     $array_paths = array(
@@ -34,6 +34,43 @@ function __autoload($class_name)
         
         if (is_file($path)){
             include_once $path;
+        }
+    }
+}*/
+
+class Autoload
+{
+    public function loadClass($class)
+    {
+        require_once KINDINFO_ROOT . '/components/PrefixDocumentRoot.php';
+        $pdr = new PrefixDocumentRoot();
+
+        $prefix_pdr = $pdr->getPDRFromat(PREFIX_DOCUMENT_ROOT, 2);
+        $arr = explode('\\', $class);
+        $prefix = array_shift($arr);
+        $file = '';
+        $prefix_file = '';
+        if ($prefix == 'components')
+        {
+            $prefix_file = '/components/';
+        }
+        elseif($prefix == 'razlom')
+        {
+            $prefix_file = '../vendor/razlom/';//.array_shift($arr).'/';
+        }
+        $file = $_SERVER['DOCUMENT_ROOT'];
+        if ($prefix_pdr != false)
+        {
+            $file .= $prefix_pdr;
+        }
+        $file .= $prefix_file.implode('/', $arr).'.php';
+        if (is_file($file))
+        {
+            require_once $file;
+        }
+        else
+        {
+            echo 'Не удалось загрузить класс';
         }
     }
 }
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+use components\Router;
+
 // Общие настройки
 error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR);
 
@@ -6,14 +9,23 @@ error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERRO
 define('KINDINFO_ROOT', dirname(__FILE__));
 define('SERVER_DOC_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
-require_once(KINDINFO_ROOT.'/components/Autoload.php');
-
 // Подключение констант
 require_once(KINDINFO_ROOT.'/config/constants.php');
 
 // Подключение констант с правами администратора
 require_once (KINDINFO_ROOT.'/config/constants_admin_access_rights.php');
 
+function GetPrefixDocumentRoot($path = null, $dir = __DIR__)
+{
+    return trim(str_replace(realpath(isset($path) ? trim($path) : $_SERVER['DOCUMENT_ROOT']), '', realpath(trim($dir))));
+}
+
+define('PREFIX_DOCUMENT_ROOT', GetPrefixDocumentRoot());
+
+require_once(KINDINFO_ROOT.'/components/Autoload.php');
+$autoload = new Autoload();
+spl_autoload_register([$autoload, 'loadClass']);
+
 // Вызов Router
-$router = new App();
+$router = new Router();
 $router->run();
